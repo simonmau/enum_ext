ec89c85113e65e35a849c132a01e2a6dcf49c6f1
Simon Mauracher <simon.mauracher@outlook.com>
2019-06-21 19:21:39 +0200

    bugfixes & cleanup

--- Enum.Ext/Enum.Ext.EFCore/ConverterExtension.cs
+++ Enum.Ext/Enum.Ext.EFCore/ConverterExtension.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Enum.Ext.EFCore
 {
