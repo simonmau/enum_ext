7913e389f00fcb50e581345e910c401ac6132289
mjuen <wr9OscZ9gY8PplO9sAW3m.juen@hotmail.com>
2019-06-21 18:13:20 +0200

    Working on extension method for easy configuration of value converters for EFCore

--- Enum.Ext/Enum.Ext.EFCore/ConverterExtension.cs
+++ Enum.Ext/Enum.Ext.EFCore/ConverterExtension.cs
@@ -1,16 +1,63 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Enum.Ext.EFCore
 {
     public static class ConverterExtension
     {
-        public static TypeSafeEnumConverter<TValue, TKey> GetEFCoreConverter<TValue, TKey>(this TypeSafeEnum<TValue, TKey> _)
-            where TValue : TypeSafeEnum<TValue, TKey>
-            where TKey : struct
+        public static void ConfigureEnumExt(this ModelBuilder modelBuilder)
         {
-            return new TypeSafeEnumConverter<TValue, TKey>();
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var properties = entityType.ClrType.GetProperties()
+                    .Where(p => IsDerived(p.PropertyType));
+
+                foreach (var property in properties)
+                {
+                    var genericArguments = GetKeyType(property.PropertyType).GetTypeInfo().GenericTypeArguments;
+
+                    var converterType = typeof(TypeSafeEnumConverter<,>)
+                       .MakeGenericType(genericArguments[0], genericArguments[1]);
+
+                    var converter = (ValueConverter)Activator.CreateInstance(converterType);
+
+                    modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion(converter);
+                }
+            }
+        }
+
+        private static bool IsDerived(Type objectType)
+        {
+            Type currentType = objectType.BaseType;
+            while (currentType != typeof(object))
+            {
+                if (currentType.IsGenericType && currentType.GetGenericTypeDefinition() == typeof(TypeSafeEnum<,>))
+                    return true;
+
+                currentType = currentType.BaseType;
+            }
+
+            return false;
+        }
+
+        private static Type GetKeyType(Type objectType)
+        {
+            Type currentType = objectType.BaseType;
+            while (currentType != typeof(object))
+            {
+                if (currentType.IsGenericType && currentType.GetGenericTypeDefinition() == typeof(TypeSafeEnum<,>))
+                    return currentType.GenericTypeArguments[1];
+
+                currentType = currentType.BaseType;
+            }
+
+            return null;
         }
     }
 }
